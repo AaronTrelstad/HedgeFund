751229ab739100f03ef8042a2adf2c6d9bd30377
Aaron Trelstad <trelstadaaron@gmail.com>
2023-05-07 19:54:32 -0500

    frontend edit

---
+++ src/components/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './main.css';
+
+const Main = () => {
+    return (
+        <>
+        <text>
+            Spartan Technologies
+        </text>
+        <h1>
+            Spartan plan on releasing its first fund "Warrior Fund" by end of 2023. This fund will use Statistical Arbitrage on a specific
+            sector to find holes in the market.
+        </h1>
+        </>
+    )
+}
+
+export default Main
