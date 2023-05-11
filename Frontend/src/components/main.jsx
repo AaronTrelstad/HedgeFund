1f834ba82bd0f721bfda3511422db48d2abb477d
Aaron Trelstad <trelstadaaron@gmail.com>
2023-05-11 09:42:25 -0500

    frontend

--- Frontend/src/components/main.jsx
+++ Frontend/src/components/main.jsx
@@ -5,19 +5,15 @@ import Nav from './nav.jsx';
 const Main = () => {
     return (
         <>
-        <h1>
-            Spartan Technologies
-        </h1>
-        <Nav/>
-        <div className='box2'>
-            <h2>
-                Spartan Technology plans on releasing its inaugural fund, "Cyclone Fund", by the end of 2023. 
-                This fund focuses on using Statisitcal Arbitrage on company pairs.
-            </h2>
-            <h2>
-                Hello
-            </h2>
-        </div>
+            <h1>
+                Website
+            </h1>
+            <Nav/>
+            <div className='box2'>
+                <h2>
+                    Hello
+                </h2>
+            </div>
         </>
     )
 }
